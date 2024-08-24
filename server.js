6cfc6b810022f3c2aa5aa225b285393a3898c770
Glenelly Karla <glenellykarlatbm@gmail.com>
2024-08-24 04:05:49 -0300

    configurando

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-import express from 'express';
-import alunosRoutes from './src/alunos/routes.js';
+import express from "express";
+import cors from "cors";
+import alunosRoutes from "./src/alunos/routes.js";
 import swaggerDocs from './swagger.js';
 
 const app = express();
 const port = 3000;
+
+// Habilitar CORS para todas as rotas
+app.use(cors());
 
 app.use(express.json());
 
@@ -16,4 +20,3 @@ app.use("/api/generation", alunosRoutes);
 swaggerDocs(app);
 
 app.listen(port, () => console.log(`app na porta ${port}`));
-
